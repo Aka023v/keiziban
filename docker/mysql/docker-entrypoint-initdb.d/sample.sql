ae996991474194f4de3f9ad4a8cd51e0084a3484
Aka023v <148590386+Aka023v@users.noreply.github.com>
2023-11-02 11:14:34 +0000

    機能の作成

--- docker/mysql/docker-entrypoint-initdb.d/sample.sql
+++ docker/mysql/docker-entrypoint-initdb.d/sample.sql
@@ -1,16 +1,16 @@
--- CREATE DATABASE IF NOT EXISTS contact;
--- USE contact;
+CREATE DATABASE IF NOT EXISTS twitter;
+USE twitter;
 
--- CREATE TABLE IF NOT EXISTS `form`
--- (
---   `id`         int(11) NOT NULL AUTO_INCREMENT,
---   `email`      varchar(255) NOT NULL,
---   `content`    text,
---   PRIMARY KEY (`id`)
--- );
+CREATE TABLE IF NOT EXISTS `tweet`
+(
+  `id`       int(11) NOT NULL AUTO_INCREMENT,
+  `poster`   varchar(255) NOT NULL,
+  `tweet`    text,
+  PRIMARY KEY (`id`)
+);
 
--- INSERT INTO `form`(`email`, `content`) VALUES
--- ('abe@gmail.com', お問い合わせテスト),
--- ('ken@gmail.com', フォームテスト),
--- ('yuto@gmail.com', 送信テスト),
--- ('tabe@gmail.com', テストテスト);
+INSERT INTO `tweet`(`poster`, `tweet`) VALUES
+('さくら', 春が来た〜),
+('もみじ', 秋が来た〜),
+('あさがお', 夏が来た〜),
+('つばき', 冬が来た〜);
--- src/index.php
+++ src/index.php
@@ -1,2 +1,35 @@
 <?php
-echo '<h1>Hello World</h1>';
+$dbh = new PDO(
+    'mysql:dbname=twitter;host=mysql;charset=utf8',
+    'root',
+    'password'
+);
+
+$query = "SELECT * FROM tweets";
+$tweets = $dbh->query($query);
+?>
+
+<body>
+  <div class="container">
+    <h1>簡易Twitter</h1>
+    <a href="./create.php">Tweetする</a><br>
+    <table border="1">
+      <tr>
+        <th>投稿者名</th>
+        <th>投稿</th>
+        <th>作成日時</th>
+        <th>編集</th>
+        <th>削除</th>
+      </tr>
+      <?php foreach ($tweets as $tweet) : ?>
+        <tr>
+          <td><?php echo $tweet['title']; ?></td>
+          <td><?php echo $tweet['content']; ?></td>
+          <td><?php echo $tweet['created_at']; ?></td>
+          <td><a href="./edit.php?id=<?php echo $tweet['id'] ?>">編集</a></td>
+          <td><a href="./delete.php?id=<?php echo $tweet['id'] ?>">削除</a></td>
+        </tr>
+      <?php endforeach; ?>
+    </table>
+  </div>
+</body>
